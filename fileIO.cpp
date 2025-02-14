17dbc0e83efef9103468cc78e8907022a2baf691
SamAllen06 <samgallen06@gmail.com>
2025-02-14 19:10:24 +0000

    fileIO working, added second csv file to test its consistency

--- fileIO.cpp
+++ fileIO.cpp
@@ -5,15 +5,17 @@
 int main(){
 	std::ifstream inFile;
 	std::string line;
+	std::string word;
 	std::stringstream converter;
 	std::string sNum1;
 	std::string sNum2;
 	int num1;
 	int num2;
+	int sum;
+	int i;
 
 	inFile.open("data.csv");
-	while(getline(inFile, line, ',')){
-		std::cout << line << std::endl;
+	while(getline(inFile, line)){
 		
 		converter.clear();
 		converter.str("");
@@ -22,14 +24,20 @@ int main(){
 
 		getline(converter, sNum1, ',');
 		getline(converter, sNum2, ',');
-		getline(converter, string, ',');
-
-		converter.clear();
-		converter.str(""); //do i need this?
-
-		std::stringstream << sNum1 << " " << sNum2 //do I need << std::endl?
-		std::stringstream >> num1 << num2
+		getline(converter, word, ',');
 		
+		converter.clear();
+		converter.str("");
+
+		converter << sNum1 << " " << sNum2;
+		converter >> num1 >> num2;
+		
+		sum = num1 + num2;
+
+		for (i = 0; i < sum; i++){
+			std::cout << word << " ";
+		} //end for loop
+		std::cout << std::endl;
 	} //end while loop
 
 	return 0;
